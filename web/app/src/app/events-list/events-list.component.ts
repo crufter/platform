c4a43bff5471a145e6962ad9b52329d97c921137
Janos Dobronszki <dobronszki@gmail.com>
2020-02-25 13:22:00 +0100

    Rebuild and events changes

--- web/app/src/app/events-list/events-list.component.ts
+++ web/app/src/app/events-list/events-list.component.ts
@@ -29,29 +29,11 @@ const eventTypesNice = {
 })
 export class EventsListComponent implements OnInit {
   @Input() events: types.Event[];
-  events1: types.Event[];
   query: string = "";
 
   constructor() {}
 
-  ngOnInit() {
-    this.events1 = [
-      {
-        type: 4,
-        timestamp: 1582623629,
-        metadata: new Map<string, string>([
-          ["commit", "0f512d34343j4h3j4h3j3h43j4h3j"],
-          ["repo", "github.com/micro/services"],
-          ["build", "466859781"]
-        ]),
-        service: {
-          name: "go.micro.srv.multi-test",
-          version: "latest",
-          source: "github.com/micro/services/multi-test"
-        }
-      }
-    ];
-  }
+  ngOnInit() {}
 
   eventTypeToString(e: types.Event): string {
     return eventTypesNice[e.type];
@@ -80,5 +62,9 @@ export class EventsListComponent implements OnInit {
       ["commit", e.metadata.get("commit")],
       ["build", e.metadata.get("build")]
     ]);
+  }
+
+  shortHash(s: string): string {
+    return s.slice(0, 8);
   }
 }
