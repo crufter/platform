0c0d2bacfc99adb801451ed7611c27fdc45df73f
Janos Dobronszki <dobronszki@gmail.com>
2020-02-25 12:39:35 +0100

    First cut of events meta

--- web/app/src/app/events-list/events-list.component.ts
+++ web/app/src/app/events-list/events-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Pipe } from "@angular/core";
 import * as types from "../types";
+import { mapToMapExpression } from "@angular/compiler/src/render3/util";
 
 const eventTypes = {
   1: "ServiceCreated",
@@ -28,13 +29,56 @@ const eventTypesNice = {
 })
 export class EventsListComponent implements OnInit {
   @Input() events: types.Event[];
+  events1: types.Event[];
   query: string = "";
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.events1 = [
+      {
+        type: 4,
+        timestamp: 1582623629,
+        metadata: new Map<string, string>([
+          ["commit", "0f512d34343j4h3j4h3j3h43j4h3j"],
+          ["repo", "github.com/micro/services"],
+          ["build", "466859781"]
+        ]),
+        service: {
+          name: "go.micro.srv.multi-test",
+          version: "latest",
+          source: "github.com/micro/services/multi-test"
+        }
+      }
+    ];
+  }
 
   eventTypeToString(e: types.Event): string {
     return eventTypesNice[e.type];
+  }
+
+  commitUrl(e: types.Event): string {
+    const repo = e.metadata.get("repo");
+    const commitHash = e.metadata.get("commit");
+    // https://github.com/micro/services/commit/f291afc98f624c44e34e758efab89e77546b709d
+    return "https://" + repo + "/commit/" + commitHash;
+  }
+
+  buildUrl(e: types.Event): string {
+    const repo = e.metadata.get("repo");
+    const buildId = e.metadata.get("build");
+    // eg. https://github.com/micro/services/runs/466859781
+    return "https://" + repo + "/runs/" + buildId;
+  }
+
+  hasMeta(e: types.Event) {
+    return (e.metadata && e.metadata.has("commit")) || e.metadata.has("build");
+  }
+
+  visibleMeta(e: types.Event): Map<string, string> {
+    return new Map([
+      ["commit", e.metadata.get("commit")],
+      ["build", e.metadata.get("build")]
+    ]);
   }
 }
